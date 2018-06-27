bf10a52894df46bcdcdc5d635f9c14e4fd3b3865
a7me63azzab <devahmedazzab2014@gmail.com>
2018-06-27 11:00:30 +0200

    postman-test api with jenkins

---
+++ autotest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo 'Testing apis with newman and jenkins .... '
+
+EnvironmentFile="Demo-OpenWeatherMap.postman_environment.json"
+
+cd postman-tests/
+
+pwd  
+
+for file in `find . -type f -name '*.postman_collection.json' -exec basename {} \;`; do
+    echo "the next file is $file"
+    newman run $file -e $EnvironmentFile
+done
